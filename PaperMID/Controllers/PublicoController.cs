e25d75c8cb80be3343bf1d0002028c5053d8a0d8
Fsociety98 <32963990+Fsociety98@users.noreply.github.com>
2018-03-24 16:28:11 -0600

    Creación

---
+++ PaperMID/Controllers/PublicoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaperMID.Models;
+using PaperMID.WebService;
+
+
+namespace PaperMID.Controllers
+{
+    public class PublicoController : Controller
+    {
+        // GET: Publico
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
